60a7aaf60baa91e52f4e592344aca6809e5f0036
Dzhabrail <dzhabulya@gmail.com>
2021-11-06 03:11:16 +0300

    refactor(project): created project structure

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EnvironmentVariables } from '@drag/config';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService<EnvironmentVariables>) => ({
+        type: 'postgres',
+        host: configService.get("DATABASE_HOST", { infer: true }),
+        // port: configService.get("DATABASE_PORT", { infer: true }),
+        username: configService.get("DATABASE_USER", { infer: true }),
+        password: configService.get("DATABASE_PASSWORD", { infer: true }),
+        database: configService.get("DATABASE_NAME", { infer: true }),
+        entities: [__dirname + '/**/*.entity.ts'],
+        synchronize: true,
+      }),
+      inject: [ConfigService]
+    })],
+    controllers: [AppController],
+    providers: [AppService],
+})
+export class AppModule {}
---
+++ src/config/config.interface.ts
@@ -0,0 +1,7 @@
+export interface EnvironmentVariables {
+  DATABASE_HOST: string;
+  DATABASE_USER: string;
+  DATABASE_NAME: string;
+  DATABASE_PASSWORD: string;
+  DATABASE_PORT: number;
+}
