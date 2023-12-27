32e31a11101bbc7690ef70b26cdaa50b27920299
Dzhabrail <dzhabulya@gmail.com>
2023-12-28 01:26:04 +0300

    refactor(test): added tests for session.service

--- src/modules/session/services/session/session.service.spec.ts
+++ src/modules/session/services/session/session.service.spec.ts
@@ -2,10 +2,12 @@ import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { just } from '@sweet-monads/maybe';
-import { DataSource } from 'typeorm';
+import { Equal } from 'typeorm';
 
 import { SessionEntity } from '../../entities/session.entity';
 import { SessionService } from './session.service';
+
+const mockUserIdentity = { ip: '127.0.0.1', userAgent: 'test user agent' };
 
 const mockUser = {
   id: 'test-id',
@@ -34,31 +36,28 @@ const mockSession = {
   sessionId: 'test-session-id',
   userAgent: 'test-user-agent',
   ip: '127.0.0.1',
-  createdAt: new Date(),
-  lastAccessAt: new Date(),
+  createdAt: new Date('2023-01-01'),
+  lastAccessAt: new Date('2022-01-01'),
   userAccountId: 'test-user-account-id',
 };
 
-/* const mockSessionWithUserAccount = { */
-/*   ...mockSession, */
-/*   userAccount: mockUser, */
-/* }; */
+const mockSessionWithUserAccount = {
+  ...mockSession,
+  userAccount: mockUser,
+};
 
 // In progress
-describe.skip('AuthJwtService', () => {
+describe('AuthJwtService', () => {
   let sessionService: SessionService;
 
   beforeEach(async () => {
+    jest.useFakeTimers().setSystemTime(new Date('2023-02-02'));
     const module: TestingModule = await Test.createTestingModule({
       imports: [ConfigModule],
       providers: [
         {
           provide: getRepositoryToken(SessionEntity),
           useValue: mockRepository,
-        },
-        {
-          provide: DataSource,
-          useClass: dataSourceMock,
         },
         SessionService,
       ],
@@ -68,14 +67,81 @@ describe.skip('AuthJwtService', () => {
   });
 
   afterEach(() => {
-    jest.clearAllMocks();
+    jest.clearAllMocks().useRealTimers();
   });
 
-  it('Returns session by id', async () => {
-    mockRepository.findOne.mockResolvedValueOnce(just(mockSession));
+  it('Return session by id and updates lastAccessAt', async () => {
+    mockRepository.findOne.mockResolvedValueOnce(mockSession);
+
+    const expectedSession = {
+      id: 'test-id',
+      sessionId: 'test-session-id',
+      userAgent: 'test-user-agent',
+      ip: '127.0.0.1',
+      createdAt: new Date('2023-01-01'),
+      lastAccessAt: new Date('2023-02-02'),
+      userAccountId: 'test-user-account-id',
+    };
 
     const result = await sessionService.getSessionById('test-session-id');
 
-    expect(result).toEqual(just(mockSession));
+    expect(mockRepository.findOne).toHaveBeenCalledWith({
+      where: { sessionId: Equal('test-session-id') },
+    });
+    expect(mockRepository.save).toHaveBeenCalledWith(expectedSession);
+    expect(result).toEqual(just(expectedSession));
+  });
+
+  it('Return session user by session id', async () => {
+    mockRepository.findOne.mockResolvedValueOnce(mockSessionWithUserAccount);
+
+    const expectedSessionUser = {
+      id: 'test-id',
+      username: 'test-username',
+      firstName: 'test-first-name',
+      lastName: 'test-last-name',
+      email: 'test-email',
+      avatarColor: 'test-avatar-color',
+    };
+
+    const result = await sessionService.getSessionUserById('test-id');
+
+    expect(mockRepository.findOne).toHaveBeenCalledWith({
+      where: { sessionId: Equal('test-id') },
+      relations: ['userAccount'],
+    });
+    expect(result).toEqual(just(expectedSessionUser));
+  });
+
+  it('Creates new session and return session-id', async () => {
+    const userId = 'test-user-id';
+
+    const result = await sessionService.createSession(userId, mockUserIdentity);
+
+    expect(mockRepository.save).toHaveBeenCalledWith({
+      userAccountId: userId,
+      sessionId: expect.any(String),
+      ...mockUserIdentity,
+    });
+
+    expect(result).toEqual({
+      sessionId: expect.any(String),
+    });
+  });
+
+  it('Deletes session by id', async () => {
+    await sessionService.deleteSession('test-id');
+
+    expect(mockRepository.delete).toHaveBeenCalledWith({
+      sessionId: Equal('test-id'),
+    });
+  });
+
+  it('Deletes all sessions by user account id', async () => {
+    await sessionService.deleteAllSessions('user-account-id');
+
+    expect(mockRepository.delete).toHaveBeenCalledWith({
+      userAccountId: Equal('user-account-id'),
+    });
   });
 });
--- src/modules/session/services/session/session.service.ts
+++ src/modules/session/services/session/session.service.ts
@@ -1,8 +1,8 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Maybe, fromNullable } from '@sweet-monads/maybe';
 import * as crypto from 'crypto';
-import { Equal, DataSource, EntityManager, Repository } from 'typeorm';
+import { Equal, Repository } from 'typeorm';
 import { v4 as uuid } from 'uuid';
 
 import { UserIdentity } from 'src/infrastructure/decorators';
@@ -10,12 +10,9 @@ import { UserIdentity } from 'src/infrastructure/decorators';
 import { SessionEntity } from '../../entities/session.entity';
 import { ISessionService } from './session.service.interface';
 
-const MAX_SESSIONS = 5;
-
 @Injectable()
 export class SessionService implements ISessionService {
   constructor(
-    @Inject(DataSource) private dataSource: DataSource,
     @InjectRepository(SessionEntity)
     private readonly sessionRepository: Repository<SessionEntity>,
   ) {}
@@ -24,26 +21,11 @@ export class SessionService implements ISessionService {
     return crypto.createHash('sha256').update(uuid()).update(crypto.randomBytes(256)).digest('hex');
   };
 
-  private async getExcessSessions(userId: string, manager: EntityManager) {
-    const [sessions, sessionsCount] = await manager.findAndCount(SessionEntity, {
-      where: { userAccountId: Equal(userId) },
-    });
-    if (sessionsCount === MAX_SESSIONS) {
-      return sessions;
-    }
-
-    return null;
-  }
-
   async getSessionUserById(sessionId: string): Promise<Maybe<SessionEntity['userAccount']>> {
     const session = await this.sessionRepository.findOne({
       where: { sessionId: Equal(sessionId) },
+      relations: ['userAccount'],
     });
-
-    if (session) {
-      session.lastAccessAt = new Date();
-      await this.sessionRepository.save(session);
-    }
 
     return fromNullable(session).map(({ userAccount }) => userAccount);
   }
@@ -53,29 +35,26 @@ export class SessionService implements ISessionService {
       where: { sessionId: Equal(sessionId) },
     });
 
+    if (session) {
+      session.lastAccessAt = new Date();
+      await this.sessionRepository.save(session);
+    }
+
     return fromNullable(session);
   }
 
   async createSession(userId: string, userIdentity: UserIdentity): Promise<{ sessionId: string }> {
-    return this.dataSource.transaction(async (transactionEntityManager) => {
-      const sessionId = this.generateSessionId();
-      const session = transactionEntityManager.create(SessionEntity, {
-        userAccountId: userId,
-        sessionId,
-        ...userIdentity,
-      });
+    const sessionId = this.generateSessionId();
 
-      await transactionEntityManager.save(SessionEntity, session);
-
-      const excessSessions = await this.getExcessSessions(userId, transactionEntityManager);
-      if (excessSessions) {
-        await transactionEntityManager.remove(excessSessions);
-      }
-
-      return {
-        sessionId,
-      };
+    this.sessionRepository.save({
+      userAccountId: userId,
+      sessionId,
+      ...userIdentity,
     });
+
+    return {
+      sessionId,
+    };
   }
 
   async deleteSession(sessionId: string): Promise<void> {
